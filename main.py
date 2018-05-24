460cb8168aecb5250dee7cdaa43a2afb5e5e1d45
stivuitorul <steveenache@gmail.com>
2018-05-24 15:11:32 +0300

    v01

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/pycharm3
 
 def main(msg):
-    #! another comment
-    #! different comment
-    #! Print a message
+    #! no more comments
     print(msg)
 
+def secondary():
+    print("New function")
 print("Hello world")
